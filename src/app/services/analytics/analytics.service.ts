a45b3ab4a1536a8ec8d394eb0fa4ebf882c6982c
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2023-03-21 09:37:06 +0200

    Add isBrowser-guards to AnalyticsService

--- src/app/services/analytics/analytics.service.ts
+++ src/app/services/analytics/analytics.service.ts
@@ -1,12 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the AnalyticsService provider.
+import { isBrowser } from 'src/standalone/utility-functions';
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
+
 @Injectable()
 export class AnalyticsService {
 
@@ -14,62 +11,71 @@ export class AnalyticsService {
   }
 
   public doAnalyticsEvent( category: any, type: any, action: any ) {
-    const path = String(document.URL).split('#')[-1];
-    try {
-      (<any>window).gtag('event', action, {
-        'event_category': category,
-        'event_label': type,
-        'event_action': action,
-        'page_path': path
-      });
-    } catch ( e ) {
-      // Fallback to UA Google Analytics
-      this.doAnalyticsEventUA(category, type, action);
+    if (isBrowser()) {
+      const path = String(document.URL).split('#')[-1];
+      try {
+        (<any>window).gtag('event', action, {
+          'event_category': category,
+          'event_label': type,
+          'event_action': action,
+          'page_path': path
+        });
+      } catch ( e ) {
+        // Fallback to UA Google Analytics
+        this.doAnalyticsEventUA(category, type, action);
+      }
     }
   }
 
   public doPageView(title: any, location?: any, path?: any) {
-    if ( !location ) {
-      location = '';
-    }
-    if ( !path ) {
-      path = String(document.URL).split('#')[-1];
-    }
-    try {
-      (<any>window).gtag('event', 'page_view', {
-        page_title: title,
-        page_location: location,
-        page_path: path
-      })
-    } catch ( e ) {
-      // Fallback to UA Google Analytics
-      this.doPageViewUA(title, location, path);
+    if (isBrowser()) {
+      if ( !location ) {
+        location = '';
+      }
+      if ( !path ) {
+        path = String(document.URL).split('#')[-1];
+      }
+      try {
+        (<any>window).gtag('event', 'page_view', {
+          page_title: title,
+          page_location: location,
+          page_path: path
+        })
+      } catch ( e ) {
+        // Fallback to UA Google Analytics
+        this.doPageViewUA(title, location, path);
+      }
     }
   }
 
   public doAnalyticsEventUA( category: any, type: any, action: any ) {
-    try {
-      (<any>window).ga('send', 'event', {
-        eventCategory: category,
-        eventLabel: type,
-        eventAction: action,
-        eventValue: 10
-      });
-    } catch ( e ) {
+    if (isBrowser()) {
+      try {
+        (<any>window).ga('send', 'event', {
+          eventCategory: category,
+          eventLabel: type,
+          eventAction: action,
+          eventValue: 10
+        });
+      } catch ( e ) {
+      }
     }
   }
 
   public doPageViewUA(title: any, location?: any, path?: any) {
-    if ( !location ) {
-      location = '';
-    }
-    if ( !path ) {
-      path = '';
-    }
-    try {
-      (<any>window).ga('set', 'page', title);
-      (<any>window).ga('send', 'pageview');
-    } catch ( e ) {
+    if (isBrowser()) {
+      if ( !location ) {
+        location = '';
+      }
+      if ( !path ) {
+        path = '';
+      }
+      try {
+        (<any>window).ga('set', 'page', title);
+        (<any>window).ga('send', 'pageview');
+      } catch ( e ) {
+      }
     }
   }
+
 }
