606f2201a1b2eaa305d7c1fe54f0b0a34b88bd24
thanh-identio <thanh@identio.fi>
2023-03-30 22:28:40 +0300

    WIP collection-side-menu: highlight, collapsible

--- src/app/components/collection-side-menu/collection-side-menu.ts
+++ src/app/components/collection-side-menu/collection-side-menu.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import {
+  Params,
+  UrlSegment
+} from "@angular/router";
 import { TableOfContentsService } from "src/app/services/toc/table-of-contents.service";
 import { config } from "src/app/services/config/config";
 
@@ -11,42 +14,52 @@ import { config } from "src/app/services/config/config";
 
 export class CollectionSideMenu {
   @Input() collectionID: string;
+  @Input() initialUrlSegments: UrlSegment[];
+  @Input() initialQueryParams: Params;
   collectionContent: any;
   isLoading: boolean = true;
   _config = config;
-  selectedMenu = {
-    collectionID: '',
-    publicationID: '',
-    chapterID: ''
-  }
+  selectedMenu: string[] = []
 
   constructor(
-    private tocService: TableOfContentsService,
-    private route: ActivatedRoute
+    private tocService: TableOfContentsService
   ) {}
 
   ngOnInit() {
-    console.log('collection side menu initialized');
-
     this.tocService.getTableOfContents(this.collectionID).subscribe(data => {
       this.collectionContent = data;
       this.isLoading = false;
       console.log('tocService response:', data);
+
+      const itemId = `${this.initialUrlSegments[1].path}_${this.initialUrlSegments[3].path}_${this.initialUrlSegments[4].path}${this.initialQueryParams.position ? `;${this.initialQueryParams.position}` : ''}`
+      this.recursiveFinding(data.children, itemId);
     });
 
-    // ! ActivatedRoute only works for components loaded via router-outlet, so this will always return an empty object since this component is outside the router-outlet defined in app.component.html.
-    // ! Find another way to get the current text IDs. We also need the position, if any.
-    // TODO: Get the current selected item IDs as @Input from app.component
-    this.route.params.subscribe({
-      next: (params) => {
-        console.log('route.params: ', params);
-        this.selectedMenu = {
-          collectionID: params.collectionID,
-          publicationID: params.publicationID,
-          chapterID: params.chapterID
-        }
-      }
-    });
   }
-
+  toggle(menuId: string, menuLevel: number) {
+    if(this.selectedMenu.includes(menuId))
+      this.selectedMenu.splice(menuLevel)
+    else if(!menuLevel){
+      this.selectedMenu = [menuId];
+    }
+    else {
+      this.selectedMenu.splice(menuLevel);
+      this.selectedMenu.push(menuId);
+    }
+  }
+  recursiveFinding(array: any[], stringForComparison: string): boolean {
+    return array.some(item => {
+      if(item.children) {
+        const res = this.recursiveFinding(item.children, stringForComparison)
+        if(res)
+          this.selectedMenu.unshift(item.itemId || item.text)
+        return res;
+      } else {
+        if(item.itemId === stringForComparison){
+          this.selectedMenu.unshift(item.itemId || item.text)
+        }
+        return item.itemId === stringForComparison;
+      }
+    })
+  }
 }
