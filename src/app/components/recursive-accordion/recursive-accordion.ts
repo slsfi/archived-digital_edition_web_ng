029c9c2c48ebc7861c1825b322ea63b9cfae8028
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2023-05-30 08:52:57 +0300

    Fix recursive accordion highlighting multiple about-page menu items in deeply nested menu trees
    The fix currently relies on somewhat ugly functions calls in the template, but works.

--- src/app/components/recursive-accordion/recursive-accordion.ts
+++ src/app/components/recursive-accordion/recursive-accordion.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
-import { CommonFunctionsService } from "../../services/common-functions/common-functions.service";
+import { CommonFunctionsService } from "src/app/services/common-functions/common-functions.service";
+
 
 type MenuChild = {
   title: string;
@@ -22,19 +23,21 @@ export class RecursiveAccordion implements OnInit {
   }
 
   ngOnInit() {
-    if(this.router.url.includes(this.parentPath)) {
-      const selectedChild = this.children.find(item => this.router.url.includes(item.id))
-      selectedChild && this.selectedMenu.push(selectedChild.id)
+    if (this.router.url.startsWith(this.parentPath)) {
+      const routerPageID = this.router.url.split('/').at(-1) || '';
+      const selectedChild = this.children.find(item => routerPageID.startsWith(item.id));
+      selectedChild && this.selectedMenu.push(selectedChild.id);
       if (selectedChild && !selectedChild.children) {
-        this.commonFunctions.setTitle(selectedChild.title, 1)
+        this.commonFunctions.setTitle(selectedChild.title, 1);
       }
     }
   }
 
   toggleAccordion(item: MenuChild) {
-    this.commonFunctions.addOrRemoveValueInArray(this.selectedMenu, item.id)
-    if(!item.children) {
-      this.commonFunctions.setTitle(item.title, 1)
+    this.commonFunctions.addOrRemoveValueInArray(this.selectedMenu, item.id);
+    if (!item.children) {
+      this.commonFunctions.setTitle(item.title, 1);
     }
   }
+
 }
