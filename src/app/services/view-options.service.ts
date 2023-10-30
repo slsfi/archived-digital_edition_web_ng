30cd2becedf7602d462418928bb525e284997d32
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2023-10-30 14:53:59 +0200

    feature: add default selected view options to config
    The view options that should be selected by default when entering a collection text page can now be set using config.ts.
    
    New config key: page.text.defaultViewOptions
    This is a string array that should contain the names of the view options that should be enabled by default. Possible values are:
    comments: true,
            personInfo
            placeInfo
            changes
            normalisations
            workInfo
            abbreviations
            paragraphNumbering
            pageBreakOriginal
            pageBreakEdition
    
    These defaults will also be applied to the collection introduction page.

--- src/app/services/view-options.service.ts
+++ src/app/services/view-options.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
+import { config } from '@config';
 import { Textsize } from '@models/textsize.model';
 
 
@@ -8,23 +9,30 @@ import { Textsize } from '@models/textsize.model';
   providedIn: 'root',
 })
 export class ViewOptionsService {
-  show = {
-    'comments': true,
-    'personInfo': false,
-    'abbreviations': false,
-    'placeInfo': false,
-    'workInfo': false,
-    'changes': false,
-    'normalisations': false,
-    'paragraphNumbering': false,
-    'pageBreakOriginal': false,
-    'pageBreakEdition': false
+  show: Record<string, boolean> = {
+    comments: false,
+    personInfo: false,
+    abbreviations: false,
+    placeInfo: false,
+    workInfo: false,
+    changes: false,
+    normalisations: false,
+    paragraphNumbering: false,
+    pageBreakOriginal: false,
+    pageBreakEdition: false
   };
 
   private epubAlertDismissed: boolean = false;
   private textsizeSubject: BehaviorSubject<Textsize> = new BehaviorSubject<Textsize>(Textsize.Small);
 
-  constructor() {}
+  constructor() {
+    const defaultViewOptions: string[] = config.page?.text?.defaultViewOptions ?? [];
+    Object.keys(this.show).forEach(key => {
+      if (defaultViewOptions.includes(key)) {
+        this.show[key] = true;
+      }
+    });
+  }
 
   getTextsize(): Observable<Textsize> {
     return this.textsizeSubject.asObservable();
