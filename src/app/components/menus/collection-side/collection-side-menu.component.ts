slsfi/archived-digital_edition_web_ng

commit 074b5bffc1ba90366dfd96efc32ac3d677143ec0

fix: set page head title of positioned TOC item to parent non-positioned TOC item title
Previously, when navigating to specific positions in a reading-text with positions, the head title of the page was set to the positioned TOC item's title. This was incorrect, because the positioned headings in the reading-text were still on the same page. After this update, the head title is set to the non-positioned parent TOC item.

For instance, in Topelius Fältskärns berättelser, navigating to the chapter "1. Slaget vid Breitenfeld" does not set the page head title to this, but rather to "Första berättelsen. Konungens Ring", since this is the title of the page where the specific chapter is just one part where the reading-text can be scrolled to.

src/app/components/menus/collection-side/collection-side-menu.component.ts
@@ -135,7 +135,7 @@ export class CollectionSideMenuComponent implements OnInit, OnChanges, OnDestroy
     this.highlightedMenu = itemId;
     const isFrontMatterPage = this.setTitleForFrontMatterPages();
     if (!isFrontMatterPage) {
-      const item = this.recursiveFinding(this.collectionMenu, itemId);
+      const item = this.recursiveFinding(this.collectionMenu, itemId.split(';')[0]);
       if (item && !this.selectedMenu.includes(item.itemId || item.nodeId)) {
         this.selectedMenu.push(item.itemId || item.nodeId);
       }

commit 8bd33cb4b3b68702cf9523faa042efd8355d8910

fix: positioned parent of selected collection side menu item not expanded
Fixes a bug introduced by https://github.com/slsfi/digital_edition_web_ng/commit/074b5bffc1ba90366dfd96efc32ac3d677143ec0

src/app/components/menus/collection-side/collection-side-menu.component.ts
@@ -135,7 +135,7 @@ export class CollectionSideMenuComponent implements OnInit, OnChanges, OnDestroy
     this.highlightedMenu = itemId;
     const isFrontMatterPage = this.setTitleForFrontMatterPages();
     if (!isFrontMatterPage) {
-      const item = this.recursiveFinding(this.collectionMenu, itemId.split(';')[0]);
+      const item = this.recursiveFindMenuItem(this.collectionMenu, itemId);
       if (item && !this.selectedMenu.includes(item.itemId || item.nodeId)) {
         this.selectedMenu.push(item.itemId || item.nodeId);
       }
@@ -175,21 +175,47 @@ export class CollectionSideMenuComponent implements OnInit, OnChanges, OnDestroy
     return itemId;
   }
 
-  private recursiveFinding(array: any[], stringForComparison: string): any {
+  /**
+   * Recursively search array for an object that has an 'itemId' property
+   * equal to 'searchItemId'. If found, the page title is set to the item's
+   * title and the item is marked as the selected item in the side menu.
+   * @param array 
+   * @param searchItemId 
+   * @param setTitleOnly if true, the matching item is not set as selected
+   * in the menu, it's title is just set as the page title.
+   */
+  private recursiveFindMenuItem(
+    array: any[],
+    searchItemId: string,
+    setTitleOnly: boolean = false
+  ): any {
     return array.find(item => {
-      if (item.itemId === stringForComparison) {
-        this.headService.setTitle([String(item.text), this.collectionTitle]);
+      if (item.itemId === searchItemId) {
+        if (item.itemId.split(';')[1]) {
+          // The itemId contains a position, so we need to find it's parent
+          // that doesn't contain a position, since we don't want positioned
+          // item's titles to be set as page titles.
+          this.recursiveFindMenuItem(
+            this.collectionMenu, item.itemId.split(';')[0], true
+          );
+        } else {
+          this.headService.setTitle([String(item.text), this.collectionTitle]);
+        }
         return item;
       } else if (item.children) {
-        const result = this.recursiveFinding(item.children, stringForComparison);
-        if (result && !this.selectedMenu.includes(result.itemId || result.nodeId)) {
+        const result = this.recursiveFindMenuItem(item.children, searchItemId);
+        if (
+          !setTitleOnly &&
+          result &&
+          !this.selectedMenu.includes(result.itemId || result.nodeId)
+        ) {
           this.selectedMenu.push(result.itemId || result.nodeId);
         }
         return result;
       } else {
         return undefined;
       }
-    })
+    });
   }
 
   /**